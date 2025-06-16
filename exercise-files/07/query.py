f23bfec60c6a4af7a08fa6f9f141cac6d5072e7a
Ernesto Orozco <ernesto.orozco.coulson@gmail.com>
2025-06-16 15:49:01 -0700

    completed

--- exercise-files/07/query.py
+++ exercise-files/07/query.py
@@ -2,9 +2,6 @@ from langchain.chains import RetrievalQA
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain_community.vectorstores import MongoDBAtlasVectorSearch
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_core.prompts import PromptTemplate
-from langchain.schema import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from pymongo import MongoClient
 from dotenv import load_dotenv
@@ -37,7 +34,7 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
 docs = text_splitter.split_documents(data)
 
 # Print the first document
-# print(docs[0])
+print(docs[0])
 
 # Instantiate the vector store
 vector_store = MongoDBAtlasVectorSearch.from_documents(
@@ -48,38 +45,15 @@ vector_store = MongoDBAtlasVectorSearch.from_documents(
 )
 
 
-llm = ChatOpenAI()
 
 # Instantiate Atlas Vector Search as a retriever
-retriever = vector_store.as_retriever(
-   search_type = "similarity",
-   search_kwargs = { "k": 1 }
-)
+
 
 # Define a prompt template
-template = """
-Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-{context}
-Question: {question}
-"""
-custom_rag_prompt = PromptTemplate.from_template(template)
  
 
 # Construct a chain to answer questions on your data
-rag_chain = (
-    {"context": retriever, "question": RunnablePassthrough() }
-    | custom_rag_prompt
-    | llm
-    |  StrOutputParser()
-)
 
 
 # Prompt the chain to answer a question
-question = "How can I secure my MongoDB Atlas cluster?"
-answer = rag_chain.invoke(question)
 
-# print(answer)
-
-print("Question: " + question)
-print("Answer: " + answer)
